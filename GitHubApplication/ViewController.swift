8082647e1af98093937f892291088135bba08a51
Станислав Шияновский <st-small@rambler.ru>
2020-02-29 19:17:00 +0200

    Initial Commit

---
+++ GitHubApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubApplication
+//
+//  Created by Stanly Shiyanovskiy on 29.02.2020.
+//  Copyright © 2020 Stanly Shiyanovskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
